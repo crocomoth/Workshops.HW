f3958145f5a6a5c4b4f5f0ea4e7be0d42ad5dff3
crocomoth <justletmeplaymf@gmail.com>
2018-06-18 18:12:24 +0300

    Changed db to localdb for testing purposes.

--- Day1. Code refactoring/Rocket.BL.Common/Services/PersonalArea/ISubscriptionService.cs
+++ Day1. Code refactoring/Rocket.BL.Common/Services/PersonalArea/ISubscriptionService.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 
-namespace Rocket.BL.Common.Services.ReleaseList
+namespace Rocket.BL.Common.Services.PersonalArea
 {
     public interface ISubscriptionService : IDisposable
     {
-    void Subscribe(string userId, int id);
+        void Subscribe(string userId, int id);
 
-    void Unsubscribe(string userId, int id);
+        void Unsubscribe(string userId, int id);
     }
 }
